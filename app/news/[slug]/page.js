0d75825c9cadfe09dff4f3117d70444a55780b6c
Hannah Z <eveline1129@163.com>
2024-04-24 17:22:22 -0500

    not found pages

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,9 +1,13 @@
 import { DUMMY_NEWS } from "@/dummy-news"
+import { notFound } from "next/navigation"
 
 export default function NewsDetailPage({ params }) {
 	const newsSlug = params.slug
 	const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug)
-
+	if (!newsItem) {
+		// otherwise nextjs would not be able to know whether a new is found or not, just the not found page is not enough
+		notFound()
+	}
 	return (
 		<article className='news-article'>
 			<header>
