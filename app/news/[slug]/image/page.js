7326bcf1a8bc3b7a889127a405db512f757a629f
Hannah Z <eveline1129@163.com>
2024-04-26 16:46:11 -0500

    Nested Routes Inside Dynamic Routes

---
+++ app/news/[slug]/image/page.js
@@ -0,0 +1,18 @@
+import { DUMMY_NEWS } from "@/dummy-news"
+import { notFound } from "next/navigation"
+
+export default function imagePage({ params }) {
+	const newsItemSlug = params.slug
+	const newsItem = DUMMY_NEWS.find((news) => news.slug === newsItemSlug)
+	if (!newsItem) {
+		notFound()
+	}
+	return (
+		<div className='fullscreen-image'>
+			<img
+				src={`/images/news/${newsItem.image}`}
+				alt={newsItem.title}
+			/>
+		</div>
+	)
+}
--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,6 +1,6 @@
 import { DUMMY_NEWS } from "@/dummy-news"
 import { notFound } from "next/navigation"
-
+import Link from "next/link"
 export default function NewsDetailPage({ params }) {
 	const newsSlug = params.slug
 	const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug)
@@ -11,10 +11,12 @@ export default function NewsDetailPage({ params }) {
 	return (
 		<article className='news-article'>
 			<header>
-				<img
-					src={`/images/news/${newsItem.image}`}
-					alt={newsItem.title}
-				/>
+				<Link href={`/news/${newsItem.slug}/image`}>
+					<img
+						src={`/images/news/${newsItem.image}`}
+						alt={newsItem.title}
+					/>
+				</Link>
 				<h1>{newsItem.title}</h1>
 				<time dateTime={newsItem.date}>{newsItem.date}</time>
 			</header>
