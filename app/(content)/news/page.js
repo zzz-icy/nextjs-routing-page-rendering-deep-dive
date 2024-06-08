b6ac13154c6b7121ceb887b811af141588e5aacd
Hannah Z <eveline1129@163.com>
2024-06-08 16:46:42 -0500

    fetching data from server

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,17 +1,18 @@
 import NewsList from "@/components/news-list"
+import { getAllNews } from "@/lib/news"
 export default async function NewsPage() {
-	const response = await fetch("http://localhost:8080/news")
+	// const response = await fetch("http://localhost:8080/news")
 
-	if (!response.ok) {
-		throw new Error("Failed to fetch news.")
-	}
+	// if (!response.ok) {
+	// 	throw new Error("Failed to fetch news.")
+	// }
 
-	const news = await response.json()
+	// const news = await response.json()
+	const news = getAllNews()
 
 	return (
 		<>
-			<p>News Page News Page</p>
-			<p>News Page News Page</p>
+			<p>News Page</p>
 			<NewsList news={news} />
 		</>
 	)
